495dc68575d92a4479575b796629337aed6897b6
Antonio Plaza <aplazaortega@gmail.com>
2018-03-04 18:15:47 +0100

    Initial RecyclerView added

--- app/src/main/java/com/kotlin/aplaza/mvpkotlin/MainActivity.kt
+++ app/src/main/java/com/kotlin/aplaza/mvpkotlin/MainActivity.kt
@@ -2,11 +2,22 @@ package com.kotlin.aplaza.mvpkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
     }
 }
