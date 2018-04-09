df4da3ffa0f3fbfb24648e7feeb14d1b757b322a
Antonio Plaza Ortega <antonio.plaza.ortega@dreamit.de>
2018-04-09 11:41:37 +0200

    Android Kotlin extesions

--- app/src/main/java/com/kotlin/aplaza/mvpkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/kotlin/aplaza/mvpkotlin/ui/MainActivity.kt
@@ -3,24 +3,22 @@ package com.kotlin.aplaza.mvpkotlin.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.kotlin.aplaza.mvpkotlin.R
 import com.kotlin.aplaza.mvpkotlin.domain.ForecastList
 import com.kotlin.aplaza.mvpkotlin.domain.RequestForecastCommand
 import com.kotlin.aplaza.mvpkotlin.ui.adapter.ForecastListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), MainView {
 
     private val presenter = MainPresenter(this)
-    private lateinit var forecastList : RecyclerView
     private var forecastAdapter = ForecastListAdapter { toast(it.date) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        forecastList = findViewById(R.id.forecast_list)
         initRecyclerView()
 
         val request = RequestForecastCommand("29018")
--- app/src/main/java/com/kotlin/aplaza/mvpkotlin/ui/adapter/ForecastListAdapter.kt
+++ app/src/main/java/com/kotlin/aplaza/mvpkotlin/ui/adapter/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.kotlin.aplaza.mvpkotlin.R
 import com.kotlin.aplaza.mvpkotlin.domain.Forecast
 import com.kotlin.aplaza.mvpkotlin.domain.ForecastList
 import com.kotlin.aplaza.mvpkotlin.ui.utils.ctx
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 /**
  * Created by home on 11/02/18.
@@ -41,19 +39,13 @@ class ForecastListAdapter(private val itemClick: (Forecast) -> Unit) :
     override fun getItemCount(): Int = weekForecast.size
 
     class ViewHolder(view: View, private val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemperatureView = view.find<TextView>(R.id.maxTemperature)
-        private val minTemperatureView = view.find<TextView>(R.id.minTemperature)
-
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "$high"
-                minTemperatureView.text = "$low"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "$high"
+                itemView.minTemperature.text = "$low"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
